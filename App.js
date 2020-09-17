ea5527d9de51dfb05e75e10abb00f453d64e09af
Palloma Gualter <pallomagualter@gmail.com>
2020-09-17 12:07:47 -0300

    commit

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.textContainer}>InstaRocket</Text>
+      <Text style={styles.textContainer}>Postgram</Text>
       <StatusBar style="auto" />
     </View>
   );
